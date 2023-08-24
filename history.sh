ad6140537e2be8d31ed0bde1152a719b1e8bf0b7
jhordyess <dev@jhordyess.com>
2023-08-24 00:54:31 +0000

    Added eslint

--- history.sh
+++ history.sh
@@ -7,3 +7,4 @@ yarn add express node-fetch querystring
 yarn add -D @types/express typescript
 yarn add -D ts-loader webpack webpack-cli webpack-node-externals nodemon-webpack-plugin dotenv-webpack
 yarn add -D prettier && touch .prettierrc && yarn prettier --write .
+yarn add -D eslint && yarn eslint --init
