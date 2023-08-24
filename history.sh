9e1518be0dbd6c1e688abd2d633a1f90d87b1119
jhordyess <dev@jhordyess.com>
2023-08-24 00:55:22 +0000

    Added husky

--- history.sh
+++ history.sh
@@ -8,3 +8,4 @@ yarn add -D @types/express typescript
 yarn add -D ts-loader webpack webpack-cli webpack-node-externals nodemon-webpack-plugin dotenv-webpack
 yarn add -D prettier && touch .prettierrc && yarn prettier --write .
 yarn add -D eslint && yarn eslint --init
+yarn add -D husky && yarn husky install && yarn husky add .husky/pre-commit "yarn lint"
