bf7e6d5f297d096784227f5014accff1a05d1795
Jhordy Gavinchu <dev@jhordyess.com>
2024-02-09 12:52:31 -0400

    Fix typo bugs

--- history.sh
+++ history.sh
@@ -4,8 +4,8 @@ yarn init -y
 
 yarn add express pug
 
-yarn add -D typescript ts-loader @types/express @types/pug @types/node 
-yarn add -D webpack webpack-cli webpack-node-externals nodemon-webpack-plugin dotenv-webpack 
+yarn add -D typescript ts-loader @types/express @types/pug @types/node
+yarn add -D webpack webpack-cli webpack-node-externals nodemon-webpack-plugin dotenv-webpack
 yarn add -D prettier && touch .prettierrc && yarn prettier --write .
 yarn add -D eslint && yarn eslint --init
 yarn add -D husky && yarn husky install && yarn husky add .husky/pre-commit "yarn lint"
