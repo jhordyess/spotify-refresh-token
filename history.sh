36027623ed9cc3870a86aa58e397f8dc2331fb17
Jhordy Gavinchu <dev@jhordyess.com>
2024-02-09 14:55:00 -0400

    Added new dependencies

--- history.sh
+++ history.sh
@@ -1,11 +1,12 @@
 #? Command history
 
-yarn init -y
+yarn init -y && npm pkg delete scripts.test
 
-yarn add express pug
+yarn add express pug morgan swagger-ui-express express-validator
 
-yarn add -D typescript ts-loader @types/express @types/pug @types/node
+yarn add -D typescript ts-loader @types/node @types/express @types/pug @types/morgan @types/swagger-ui-express
 yarn add -D webpack webpack-cli webpack-node-externals nodemon-webpack-plugin dotenv-webpack
+
 yarn add -D prettier && touch .prettierrc && yarn prettier --write .
 yarn add -D eslint && yarn eslint --init
 yarn add -D husky && yarn husky install && yarn husky add .husky/pre-commit "yarn lint"
